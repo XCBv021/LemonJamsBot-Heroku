615ae059775be71f8703a9e4e8027ddbaee8bb29
XCBZx021 <61191296+XCBv021@users.noreply.github.com>
2021-02-19 16:06:27 +0530

    Update index.ts

--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -4,10 +4,12 @@ import { playHandler } from './play';
 import { queueHandler } from './queue';
 import { pauseHandler } from './pause';
 import { skipHandler } from './skip';
+import { idsHandler } from './ids';
 
 export const initHandlers = (): void => {
     bot.use(playHandler);
     bot.use(queueHandler);
     bot.use(pauseHandler);
     bot.use(skipHandler);
+    bot.use(idsHandler);
 };
